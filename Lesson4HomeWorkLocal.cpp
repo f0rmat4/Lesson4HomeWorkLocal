5977b748f15ed8d01e0f6e8e4fec023cf4880fd3
f0rmat4 <f0rmat4you@gmail.com>
2021-02-12 03:07:50 +0300

    Это пока всё на что меня хватило, но всё равно не работает как надо по заданию =((

--- Lesson4HomeWorkLocal.cpp
+++ Lesson4HomeWorkLocal.cpp
@@ -1,4 +1,35 @@
 #include <iostream>
-int main()
-{
+#include <cmath>
+#include <Windows.h>
+using namespace std;
+
+
+
+int main(int argc, const char* argv[]) {
+
+	double a, b;
+	double sum = a + b;
+	cin >> a >> b;
+
+
+	if ((sum >= 10) && (sum <= 20)) {
+		cout << "true" << endl;
+	}
+
+
+
+	int x, y = x % x;
+	cout << "Enter number" << endl;
+	cin >> x;
+	while (y == x > 0) {
+
+		cout << "Natural number" << endl;
+	}
+	cout << "NOT Natural number" << endl;
+
+
 }
+
+			
+			
+
